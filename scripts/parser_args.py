a55b682754ecbdedfa75870e47dab31644ec6488
Kenenbek R <kenenbek@gmail.com>
2020-07-22 00:09:30 +0300

    separate arguments handling

---
+++ scripts/parser_args.py
@@ -0,0 +1,47 @@
+
+def gru_add_arguments(parser):
+    parser.add_argument('--input_size', type=int, default=1)
+    parser.add_argument('--out_channels', type=int, default=128)
+    parser.add_argument('--kernel_size', type=int, default=1001)
+    parser.add_argument('--hidden_size', type=int, default=64)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--batch_first', type=bool, default=True)
+    parser.add_argument('--bidirectional', type=bool, default=True)
+    parser.add_argument('--dropout', type=float, default=0.2)
+
+
+def conv_add_arguments(parser):
+    parser.add_argument('--emb_size_conv', type=int)
+    parser.add_argument('--hidden_size_conv', type=int)
+    parser.add_argument('--kernel_size', type=int)
+    parser.add_argument('--num_layers_conv', type=int)
+    parser.add_argument('--dropout', type=float)
+
+
+def bert_add_arguments(parser):
+    parser.add_argument("--input_size", type=int)
+    parser.add_argument("--n_token_in", type=int)
+    parser.add_argument("--hidden_size_bert", type=int)
+    parser.add_argument("--num_layers_bert", type=int)
+    parser.add_argument("--num_attention_heads", type=int)
+    parser.add_argument("--intermediate_size", type=int)
+    parser.add_argument("--hidden_dropout_prob", type=float)
+    parser.add_argument("--attention_probs_dropout_prob", type=float)
+    parser.add_argument("--type_vocab_size", type=int)
+    parser.add_argument("--initializer_range", type=float)
+    parser.add_argument("--layer_norm_eps", type=float)
+    parser.add_argument("--pad_token_id", type=int)
+    parser.add_argument("--gradient_checkpointing", type=bool)
+
+
+def conv_bert_add_arguments(parser):
+    conv_add_arguments(parser)
+    bert_add_arguments(parser)
+
+
+def gru_one_dir_add_arguments(parser):
+    parser.add_argument('--input_size', type=int, default=1)
+    parser.add_argument('--hidden_size', type=int, default=64)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--batch_first', type=bool, default=True)
+    parser.add_argument('--dropout', type=float, default=0.1)
