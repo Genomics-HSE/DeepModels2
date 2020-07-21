95eb3a88b2066f8fd643192750e7cc742d095d8a
Kenenbek R <kenenbek@gmail.com>
2020-07-22 00:11:31 +0300

    add bert model

--- src/genomics_models/bert.py
+++ src/genomics_models/bert.py
@@ -7,12 +7,11 @@ class Model:
     def __new__(cls, args):
         return EncoderBert(
             seq_len=args.seq_len,
-            vocab_size=args.n_input,
-            hidden_size=args.hidden_size,
-            num_hidden_layers=args.num_hidden_layers,
+            vocab_size=args.n_token_in,
+            hidden_size=args.hidden_size_bert,
+            num_hidden_layers=args.num_layers_bert,
             num_attention_heads=args.num_attention_heads,
             intermediate_size=args.intermediate_size,
-            max_position_embeddings=args.max_position_embeddings,
             num_labels=args.n_output
         ).to(args.device)
 
@@ -22,23 +21,24 @@ class EncoderBert(nn.Module):
                  vocab_size=2, hidden_size=196, num_hidden_layers=12,
                  num_attention_heads=12, intermediate_size=1024, hidden_act='relu',
                  hidden_dropout_prob=0.1, attention_probs_dropout_prob=0.1,
-                 max_position_embeddings=10000, type_vocab_size=2, initializer_range=0.02,
-                 layer_norm_eps=1e-12, pad_token_id=0, gradient_checkpointing=False, **kwargs):
+                 type_vocab_size=1, initializer_range=0.02, layer_norm_eps=1e-12,
+                 pad_token_id=0, gradient_checkpointing=False, num_labels=20, **kwargs):
         super().__init__()
         
         self.seq_len = seq_len
         self.hidden_size = hidden_size
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
+        self.n_output = num_labels
         
         config = BertConfig(
             vocab_size=vocab_size, hidden_size=hidden_size, num_hidden_layers=num_hidden_layers,
             num_attention_heads=num_attention_heads, intermediate_size=intermediate_size,
             hidden_act=hidden_act, hidden_dropout_prob=hidden_dropout_prob,
             attention_probs_dropout_prob=attention_probs_dropout_prob,
-            max_position_embeddings=max_position_embeddings, type_vocab_size=type_vocab_size,
+            max_position_embeddings=seq_len, type_vocab_size=type_vocab_size,
             initializer_range=initializer_range, layer_norm_eps=layer_norm_eps, pad_token_id=pad_token_id,
-            gradient_checkpointing=gradient_checkpointing, **kwargs
+            gradient_checkpointing=gradient_checkpointing, num_labels=num_labels, **kwargs
         )
         
         self.bert_model = BertForTokenClassification(config)
@@ -52,9 +52,11 @@ class EncoderBert(nn.Module):
         :return:
         """
         input = input.float()
-        input = input.unsqueeze(1)
+        input = input.unsqueeze(2)
         # (batch_size, seq_len, 1)
-        output = self.bert_model(input)
+        print(input.shape)
+        output, *h = self.bert_model(inputs_embeds=input)
+        print(output.shape)
         return output,
     
     @property
--- src/genomics_models/conv_layer.py
+++ src/genomics_models/conv_layer.py
@@ -1,12 +1,24 @@
-import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-class ConvLayer(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
-        conv = nn.Conv1d(in_channels=in_channels,
-                         out_channels=out_channels,
-                         kernel_size=kernel_size,
-                         stride=stride)
+class ConvLayer(nn.Module):
+    def __init__(self, input_size, out_channels, kernel_size, stride, dropout):
+        super().__init__()
+        self.conv1d = nn.Conv1d(in_channels=input_size,
+                                out_channels=out_channels,
+                                kernel_size=kernel_size,
+                                stride=stride)
         
+        self.batch_norm = nn.BatchNorm1d(num_features=out_channels)
+        self.dropout0 = nn.Dropout(dropout)
+    
+    def forward(self, input):
+        # (batch_size, input_channels, pad_seq_len)
+        output = self.conv1d(input)
+        # (batch_size, out_channels, seq_len)
+        output = self.batch_norm(output)
+        output = F.relu(output)
+        output = self.dropout0(output)
         
+        return output
