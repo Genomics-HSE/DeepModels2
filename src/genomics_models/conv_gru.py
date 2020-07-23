5deb3d7244eba49fb77633bc4e09ed086a720f9e
Kenenbek R <kenenbek@gmail.com>
2020-07-24 01:53:31 +0300

    create new model

---
+++ src/genomics_models/conv_gru.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+from .gru import EncoderGRU
+from .conv_layer import ConvLayer
+
+
+class Model:
+    def __new__(cls, args):
+        return EncoderConvGRU(
+            seq_len=args.seq_len,
+            vocab_size=args.n_token_in,
+            hidden_size=args.hidden_size_bert,
+            num_hidden_layers=args.num_layers_bert,
+            num_attention_heads=args.num_attention_heads,
+            intermediate_size=args.intermediate_size,
+            num_labels=args.n_output
+        ).to(args.device)
+
+
+class EncoderConvGRU(nn.Module):
+    def __init__(self):
+      
+      self.embedding = ConvLayer(in_channels=1,
+                                 out_channels=hidden_size,
+                                 kernel_size=kernel_size,
+                                 stride=1,
+                                 padding=0,
+                                 dropout=dropout
+                                 )
