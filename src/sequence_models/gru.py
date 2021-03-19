ff9ea4d214fd5f9edaee49d5b8a3ac0cbda093cf
Kenenbek R <kenenbek@gmail.com>
2021-03-19 18:53:05 +0300

    checkpoint model

--- src/sequence_models/gru.py
+++ src/sequence_models/gru.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils import checkpoint
+import torch.utils.checkpoint as cp
 
 from genomics_utils import LightningModuleExtended
 
@@ -20,6 +20,17 @@ class Model:
                        )
 
 
+class GruDummyInput(nn.Module):
+    def __init__(self, gru_module):
+        super().__init__()
+        self.gru_module = gru_module
+    
+    def forward(self, input, hiddens, dummy_input=None):
+        assert dummy_input is not None
+        output, hiddens = self.gru_module(input, hiddens)
+        return output, hiddens
+
+
 class Encoder(LightningModuleExtended):
     def __init__(self, seq_len, input_size, hidden_size, num_layers,
                  batch_first, bidirectional, dropout, n_output, t_bptt):
@@ -33,25 +44,29 @@ class Encoder(LightningModuleExtended):
         self.n_output = n_output
         self.t_bptt = t_bptt
         
-        self.gru = nn.GRU(input_size=input_size,
-                          hidden_size=hidden_size,
-                          num_layers=num_layers,
-                          batch_first=batch_first,
-                          bidirectional=bidirectional,
-                          dropout=dropout
-                          )
+        gru_module = nn.GRU(input_size=input_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=batch_first,
+                            bidirectional=bidirectional,
+                            dropout=dropout
+                            )
+        
+        self.gru = GruDummyInput(gru_module)
         
         self.dense1 = nn.Linear((1 + bidirectional) * hidden_size, hidden_size)
         self.dropout1 = nn.Dropout(dropout)
         self.dense2 = nn.Linear(hidden_size, n_output)
+        
+        self.dummy_tensor = torch.ones(1, dtype=torch.float32, requires_grad=True)
     
     def forward(self, input, hiddens):
         """
-        :param input: (batch_size, seq_len, 1)
-        :return:
-        """
+            :param input: (batch_size, seq_len, 1)
+            :return:
+            """
         input = input.float()
-        output, hiddens = self.gru(input, hiddens)
+        output, hiddens = cp.checkpoint(self.gru, *(input, hiddens, self.dummy_tensor))
         
         output = self.dropout1(F.relu(self.dense1(output)))
         output = F.log_softmax(self.dense2(output), dim=-1)
@@ -59,10 +74,10 @@ class Encoder(LightningModuleExtended):
     
     @property
     def name(self):
-        return 'Gen{}-tbptt{}-GRU-sl{}-hs{}-nl{}-bidir{}'.format(self.n_output,
-                                                                 self.t_bptt,
-                                                                 self.inp_seq_len,
-                                                                 self.hidden_size,
-                                                                 self.num_layers,
-                                                                 self.bidirectional
-                                                                 )
+        return 'Gen{}-tbptt{}-GRU-sl{}-hs{}-nl{}-dir{}'.format(self.n_output,
+                                                               self.t_bptt,
+                                                               self.inp_seq_len,
+                                                               self.hidden_size,
+                                                               self.num_layers,
+                                                               1 + int(self.bidirectional)
+                                                               )
